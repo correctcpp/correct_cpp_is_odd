b21afbd121e844fde192fbc2b7bf7818f4e861ad
correctcpp <correctcpp@richelbilderbeek.nl>
2018-01-13 13:16:49 +0100

    Added documentation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 #include <string>
 #include <vector>
 
+/// Program to determine if a number is odd.
+/// If there are no arguments supplied by the user,
+/// exit code will be 1.
+/// If there are two or more arguments supplied by the
+/// user, only the first one will be used
+/// The first argument is tested to be an odd integer
+/// number. If the argument is not a number, or too
+/// big to be converted to a number, exit code will
+/// be one. If the number is odd, 'true' (and a newline)
+/// will be displayed, 'false' (and a newline) otherwise
 int main(int argc, char* argv[])
 {
   const std::vector<std::string> args(argv, argv + argc);
