4a59228fb356bcc2244ae989ef773836e2ca872e
correctcpp <correctcpp@richelbilderbeek.nl>
2018-01-13 13:14:23 +0100

    Done exercise

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
-main(argc, argv)
-{
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
 
+int main(int argc, char* argv[])
+{
+  const std::vector<std::string> args(argv, argv + argc);
+  assert(argc >= 1);
+  if (argc == 1) return 1;
+  try
+  {
+    const int number{std::stoi(args[1])};
+    if (number % 2)
+    {
+      std::cout << "true\n";
+    }
+    else
+    {
+      std::cout << "false\n";
+    }
+  }
+  catch(const std::exception&)
+  {
+    return 1;
+  }
 }
